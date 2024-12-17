1310ff9dbc4e00eecec98999de0c056b26de4cca
erysdren <contact@erysdren.me>
2024-12-17 07:09:03 -0600

    add defaultwall.glsl

---
+++ rott/glsl/defaultwall.glsl
@@ -0,0 +1,58 @@
+!!ver 130 460
+
+!!permu FULLBRIGHT
+!!permu FOG
+!!permu LIGHTSTYLED
+
+!!samps diffuse lightmap fullbright
+
+varying vec2 txc;
+varying vec2 lmc;
+
+#include "sys/defs.h"
+#include "sys/fog.h"
+
+#ifdef VERTEX_SHADER
+
+	void main()
+	{
+		txc = v_texcoord;
+		lmc = v_lmcoord;
+
+		gl_Position = ftetransform();
+	}
+
+#endif
+
+#ifdef FRAGMENT_SHADER
+
+	void main()
+	{
+		// diffuse sampler
+		vec4 diffuse = texture2D(s_diffuse, fract(txc));
+
+		// apply lightmap and brightmap if texture is not unlit
+		#if !defined(UNLIT)
+
+			// apply lightmap
+			vec2 lmsize = vec2(textureSize(s_lightmap, 0));
+			vec2 nlmc = floor(lmc * lmsize * 16) / (lmsize * 16);
+			vec3 lightmaps = (texture2D(s_lightmap, nlmc) * e_lmscale).rgb;
+			diffuse.rgb *= lightmaps.rgb;
+
+			// apply brightmap
+			#if defined(FULLBRIGHT)
+
+				vec4 brightmap = texture2D(s_fullbright, tc);
+				diffuse = brightmap * brightmap.a + diffuse * (1.0 - brightmap.a);
+
+			#endif
+
+		#endif
+
+		if (diffuse.a < 0.1) discard;
+
+		gl_FragColor = fog4(diffuse * e_colourident);
+	}
+
+#endif
